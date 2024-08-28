68463c9553cee2a885d0a33a295fe6501e539484
walterzl <52513451+walterzl@users.noreply.github.com>
2024-08-28 01:13:27 -0400

    Update conexion.php

--- config/conexion.php
+++ config/conexion.php
@@ -28,7 +28,7 @@
 
         public static function ruta(){
             /* Ruta principal del proyecto se centraliza desde aqui toda la ruta principal del proyecto */
-			return "https://ticket.ranco.cl/GestionRanco/";
+			return "ticketsisor.up.railway.app/GestionRanco/";
             /* return "https://testing.ticket.ranco.cl/GestionRanco2/"; */
             /* return "http://localhost/GestionRanco2/"; */
             
